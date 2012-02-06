8d8f3e48571d1993184cb538b7c959ffbd7757c2
Adrià Cereto-Massagué <adrian.cereto@urv.cat>
2012-02-06 10:52:58 +0100

    purge unused functions and translate comments

--- PDBfiles.py
+++ PDBfiles.py
@@ -22,7 +22,7 @@ inchidict = None
 
 def get_pdb_file(pdbcode, filename = None):
     """
-    Es descarrega un fitxer del PDB
+    Downloads a PDB file and stores it with the specified filename
     """
     url = PDBbase % pdbcode
 
@@ -52,138 +52,13 @@ def get_pdb_file(pdbcode, filename = None):
             return
             tries = 999
         except Exception, e:
-            print "No s'ha pogut descarregar",  url
-            print "Tornant-ho a intentar...",  tries
+            print "Could not download",  url
+            print "Retrying...",  tries
             time.sleep(1)
     if tries > 3:
-        print "No s'ha descarregat %s !!!!" % url
-
-def get_pdbs_from_SP(pdbrefsdict, download_all_pdb = False):
-    """
-    Descarrega els fitxers del PDB els codis dels quals estàn continguts en una llista:
-    [id, swissprot accn, pdb id] i els ordena per carpetes segons el swissprot accn
-    """
-    #####################################################
-
-    #####################################################
-    print 'Obtenint fitxers del pdb... (això pot trigar una estona)'
-    pool = multiprocessing.Pool(multiprocessing.cpu_count()*20 + 1)
-    pool.map(download_pdb_to_accn_dir, pdbrefsdict.values())
-    pool.close()
-    pool.join()
-
-
-def download_pdb_to_accn_dir(pdbdict):
-    """
-    Desa un fitxer del pdb sota un directori amb el nom de la seva fitxa corresponent del SwissProt
-    """
-    saveto = os.path.join(PREFIX,pdbdict['UNIPROT_ACCN'])
-    basename = pdbdict['PDB_ID'] + ".pdb.gz"
-    if not os.path.isdir(saveto):
-        try:
-            os.makedirs(saveto)
-        except OSError,  e:
-            print e
-            #Potser s'ha fet des d'un altre fil
-    filename = os.path.join(saveto, basename)
-    get_pdb_file(pdbdict['PDB_ID'],filename)
+        print "%s could not be downloaded!!!!" % url
 
 ########################################################
-
-def parse_query_row(row):
-    """
-    Funció dedicada a buscar llligands dins dels fitxers pdb a partir de la consulta a la base de dades
-    """
-    pdb_id, accn = row
-    pdbdictlist = []
-    liganddictdict = {}
-    if hetdict.has_key(pdb_id.lower()):
-        for ligand in hetdict[pdb_id.lower()]:
-            pdbdict = {'PDB_ID':pdb_id, 'HET_ID': ligand}
-            pdbgz = os.path.join(PREFIX, accn, pdb_id + ".pdb.gz")
-            if  os.path.isfile(pdbgz):
-                pdb_contents = parse_pdb_file(pdbgz)
-                if  pdbdict['HET_ID'] not in pdb_contents['SEQRES']:
-                    pdbdict['COVALENTLY_BOUND'] = 'FALSE'
-                    if pdb_contents.has_key('LINK'):
-                        for res1,  res2,  blen in pdb_contents['LINK']:
-                            if blen and float(blen) <= 1.9\
-                            and (res1 not in ligand_blacklist and res2 not in ligand_blacklist)\
-                            and  ((res1 not in pdb_contents['SEQRES'] and res2 in pdb_contents['SEQRES'])\
-                            or (res1 in pdb_contents['SEQRES'] and res2 not in pdb_contents['SEQRES'])):
-                                pdbdict['COVALENTLY_BOUND'] = 'TRUE'
-                    if 'INHIB' in pdb_contents['TITLE'].upper():
-                        pdbdict['IS_INHIBITOR'] = 'TRUE'
-                    else:
-                        pdbdict['IS_INHIBITOR'] = 'FALSE'
-                    pdbdictlist.append(pdbdict)
-                    if inchidict.has_key(ligand):
-                        inchikey = inchidict[ligand][1]
-                        inchi = inchidict[ligand][0]
-                        hetnam = inchidict[ligand][2]
-                        if liganddictdict.has_key(inchikey):
-                            liganddict = liganddictdict[inchikey]
-                        else:
-                            liganddict = {}
-                        liganddict['InChI'] = inchi
-                        liganddict['HET_ID'] = ligand
-                        liganddict['InChIkey'] = inchikey
-                        liganddict['HETNAM'] = hetnam
-                        if pdb_contents.has_key('HETSYN'):
-                            for hetsyn in pdb_contents['HETSYN']:
-                                if hetsyn[0] == ligand:
-                                    liganddict['HETSYN'] = hetsyn[1]
-                        liganddictdict[inchikey] = liganddict
-    return pdbdictlist, liganddictdict
-
-
-def parse_pdb_file(pdbgz):
-    """Ha de ser en .gz"""
-    pdb_contents = {'TITLE':'', 'SEQRES':set()}
-    pdbfile = gzip.GzipFile(pdbgz)
-    for line in pdbfile:
-        line = line.strip()
-        content = None
-        if len(line) < 6:
-            continue
-        label = line[:6].strip()
-        if label == 'LINK':
-            content = (line[17:20].strip(),  line[47:50].strip(),  line[73:78].strip()) #distancia
-        elif label == 'SEQRES':
-            content = set(line[19:].split())
-        elif label == 'TITLE':
-            content = line[6:].strip()
-        elif label in ('HETNAM', 'HETSYN'):
-            if not line[6:11].strip():
-                content = [line[11:14].strip() ,  line[14:].strip()]
-            else:
-                content = None
-                if line[11:14].strip() == pdb_contents[label][-1][0]:
-                    pdb_contents[label][-1][1] += line[14:]
-                    pdb_contents[label][-1][1] = pdb_contents[label][-1][1].strip()
-                    while '  ' in pdb_contents[label][-1][1]:
-                        pdb_contents[label][-1][1] = pdb_contents[label][-1][1].replace('  ', ' ')
-                    while '- ' in pdb_contents[label][-1][1]:
-                        pdb_contents[label][-1][1] = pdb_contents[label][-1][1].replace('- ', '-')
-                    while ' -' in pdb_contents[label][-1][1]:
-                        pdb_contents[label][-1][1] = pdb_contents[label][-1][1].replace(' -', '-')
-                else:
-                    print('Error: format inesperat')
-        if content:
-            if not pdb_contents.has_key(label):#Primer registre LINK
-                if label != 'SEQRES':
-                    content = [content]
-                pdb_contents[label] = content
-            else:
-                if label == 'SEQRES':
-                    pdb_contents[label].update(content)
-                elif label == 'TITLE':
-                    pdb_contents[label] += ' ' + content
-                elif label in ('HETNAM', 'HETSYN', 'LINK'):
-                    pdb_contents[label].append(content)
-    pdbfile.close()
-    return pdb_contents
-
 
 def get_ligand_pdb_dict(blacklist = True):
     """Returns a pdb code - ligands dictionary"""
@@ -234,8 +109,9 @@ def setglobaldicts():
     """
     """
     global hetdict
-    global inchidict
-    hetdict = get_ligand_pdb_dict()
+    #global inchidict
+    if not hetdict:
+        hetdict = get_ligand_pdb_dict()
     #inchidict = get_inchi_list()
 
 if __name__ == "__main__":
