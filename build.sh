666bfaaf74561e6c67e0fc80b1b0d2e3f5d9202a
Adrià Cereto-Massagué <adrian.cereto@urv.cat>
2012-10-16 12:17:21 +0200

    updated build file

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 OLDDIR=`pwd`
 rm *py.class
-CLASSPATH=one-jar/lib/Jmol.jar jython -c 'import visualitzador'
+java -jar one-jar/lib/jython.jar -c'import sys; sys.path.append("one-jar/src/pylib/"); sys.path.append("one-jar/lib/Jmol.jar"); import visualitzador'
 cd one-jar
 ant clean
 ant
