61f09595d78cbbff73489404c177c23463f1b4f9
Adrià Cereto-Massagué <adrian.cereto@urv.cat>
2012-11-08 12:52:58 +0100

    Make build process more portable

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 OLDDIR=`pwd`
-rm *py.class
+rm *.class
 javac PdbAtomJava.java
 java -jar one-jar/lib/jython.jar -c'import sys; sys.path.append("one-jar/src/pylib/"); sys.path.append("one-jar/lib/Jmol.jar"); import visualitzador'
+cp -vr *.class *.png swingutils one-jar/src/
+cp -v Main.java  one-jar/java-src/
 cd one-jar
 ant clean
 ant
