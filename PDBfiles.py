467d999fe5e6e3b2c85b6c450f9e95d1562c6049
Adrià Cereto-Massagué <ssorgatem@gmail.com>
2013-01-04 15:30:06 +0100

    started work on PDB_redo support

--- PDBfiles.py
+++ PDBfiles.py
@@ -13,42 +13,44 @@ else:
 from cofactors import ligand_blacklist
 
 PDBbase = "http://www.rcsb.org/pdb/files/%s.pdb.gz"
+PDBREDObase = "http://www.cmbi.ru.nl/pdb_redo/MIDDLE/PDBID/PDBID_besttls.pdb"
 #On guardarem els fitxers:
 CACHEDIR = tempfile.mkdtemp()
 
-def get_pdb_file(pdbcode, filename = ''):
+def get_pdb_file(pdbcode, filename = '', pdb_redo = False):
     """
     Downloads a PDB file and stores it with the specified filename
     """
     global CACHEDIR
     if not os.path.isdir(CACHEDIR):
         os.makedirs(CACHEDIR)
-    url = PDBbase % pdbcode
-
-    if not filename:
-        filename = os.path.join(CACHEDIR, pdbcode.upper() + ".pdb.gz")
+    if not pdb_redo:
+        url = PDBbase % pdbcode
+        if not filename:
+            filename = os.path.join(CACHEDIR, pdbcode.upper() + ".pdb.gz")
+    else:
+        url = PDBREDObase.replace('MIDDLE', pdbcode[1:3]).replace('PDBID', pdbcode)
+        if not filename:
+            filename = os.path.join(CACHEDIR, pdbcode.upper() + ".pdb")
+    if os.path.isfile(filename):
+        return filename
     tries = 0
-    downloaded = False
-    while tries <= 3 and not downloaded:
+    while tries <= 3:
         tries += 1
         try:
-            if os.path.isfile(filename):
-                return filename
             handler = urllib2.urlopen(url)
             filehandle = open(filename, "wb")
             filehandle.write(handler.read())
             filehandle.close()
             handler.close()
-            downloaded = True
             return filename
         except Exception, e:
             print "Could not download",  url
             print e
             print "Retrying...",  tries
             time.sleep(1)
-    if tries > 3:
-        print "%s could not be downloaded!!!!" % url
-        return ''
+    print "%s could not be downloaded!!!!" % url
+    return ''
 
 ########################################################
 
