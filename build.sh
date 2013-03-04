e0c09d93995cde0f35d174b0b229f11107071fd9
Adrià Cereto-Massagué <ssorgatem@gmail.com>
2013-03-04 13:54:07 +0100

    build with version string in final filename

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 OLDDIR=`pwd`
 rm *.class
 javac PdbAtomJava.java
-java -jar one-jar/lib/jython.jar -c'import sys; sys.path.append("one-jar/src/pylib/"); sys.path.append("one-jar/lib/Jmol.jar"); import visualitzador'
+version=$(java -jar one-jar/lib/jython.jar -c'import sys; sys.path.append("one-jar/src/pylib/"); sys.path.append("one-jar/lib/Jmol.jar"); import visualitzador; sys.stdout.write(visualitzador.VHELIBS_VERSION)')
 cp -vr *.class *.png swingutils one-jar/src/
 cp -v Main.java  one-jar/java-src/
 cd one-jar
 ant clean
 ant
+mv VHELIBS.jar VHELIBS-$version.jar
 cd $OLDDIR
 
 python setup.py build_ext --inplace
