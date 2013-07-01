14c7f4a9715df9361ff490fd3515169323bd105c
Adrià Cereto-Massagué <adrian.cereto@urv.cat>
2013-07-01 13:22:22 +0200

    bump version number

--- visualitzador.py
+++ visualitzador.py
@@ -55,7 +55,7 @@ from org.jmol.adapter.smarter import SmarterJmolAdapter
 from org.jmol.api import JmolViewer
 from org.openscience.jmol.app.jmolpanel import AppConsole
 
-VHELIBS_VERSION = "3.0.7"
+VHELIBS_VERSION = "3.0.8"
 TITLE =  "VHELIBS " + VHELIBS_VERSION
 
 #Own stuff
