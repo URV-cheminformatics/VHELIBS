ade2e0c3bc8106f62b55dc0e407e7d837ea0b1ac
Adrià Cereto-Massagué <ssorgatem@gmail.com>
2024-01-10 17:04:41 +0100

    version bump

--- visualitzador.py
+++ visualitzador.py
@@ -55,7 +55,7 @@ from org.jmol.adapter.smarter import SmarterJmolAdapter
 from org.jmol.api import JmolViewer
 from org.openscience.jmol.app.jmolpanel.console import AppConsole
 
-VHELIBS_VERSION = "5.0"
+VHELIBS_VERSION = "5.1"
 TITLE =  "VHELIBS " + VHELIBS_VERSION
 
 #Own stuff
