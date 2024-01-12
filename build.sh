4b78928b8189ea050343a0caa000b4657262622a
Adrià Cereto-Massagué <ssorgatem@gmail.com>
2024-01-12 15:35:47 +0100

    add pdbx to the build script

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ OLDDIR=`pwd`
 rm *.class
 javac PdbAtomJava.java
 version=$(java -Dpython.import.site=false -jar one-jar/lib/jython.jar -c'import sys; sys.path.append("one-jar/src/pylib/"); sys.path.append("one-jar/lib/Jmol.jar"); import visualitzador; sys.stdout.write(visualitzador.VHELIBS_VERSION)')
-cp -vr *.class *.png swingutils one-jar/src/
+cp -vr *.class *.png swingutils pdbx one-jar/src/
 cp -v Main.java  one-jar/java-src/
 cd one-jar
 ant clean
