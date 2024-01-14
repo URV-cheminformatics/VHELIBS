5608d60fd3c2b15037dbcaaca2fba8c962afb4a2
Adrià Cereto-Massagué <ssorgatem@gmail.com>
2024-01-14 22:57:27 +0100

    make cython module buildable

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ cflags=["-O3"]
 setup(
     cmdclass = {'build_ext': build_ext},
     ext_modules = [Extension("cPdbAtom", ["cPdbAtom.pyx"],extra_compile_args=cflags)
-                  ,Extension("PDBfiles", ["PDBfiles.py", "PDBfiles.pxd"],extra_compile_args=cflags)
-                  ,Extension("EDS_parser", ["EDS_parser.py", "EDS_parser.pxd"],extra_compile_args=cflags)
-                  ,Extension("cofactors", ["cofactors.py","cofactors.pxd"],extra_compile_args=cflags)
+                  # ,Extension("PDBfiles", ["PDBfiles.py", "PDBfiles.pxd"],extra_compile_args=cflags)
+                  # ,Extension("EDS_parser", ["EDS_parser.py", "EDS_parser.pxd"],extra_compile_args=cflags)
+                  # ,Extension("cofactors", ["cofactors.py","cofactors.pxd"],extra_compile_args=cflags)
                   ]
 )
